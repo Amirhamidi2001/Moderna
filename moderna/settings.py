2c1cfe09b9ff4183ae1d55cc9befbcee659b6bed
Amir Hamidi <Amirarsalanh2001@gmail.com>
2022-11-04 11:45:14 +0330

    Added static files

--- moderna/settings.py
+++ moderna/settings.py
@@ -113,6 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "static/"
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+
+# Media files (Images)
+MEDIA_URL = "media/"
+MEDIA_ROOT = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
