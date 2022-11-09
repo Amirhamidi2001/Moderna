39b9f978e3e6fe841e83a97f848b164b83b72dcd
Amir Hamidi <Amirarsalanh2001@gmail.com>
2022-11-09 16:15:22 +0330

    update models and admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """This class """
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'author', 'counted_views', 'status')
+    list_filter = ('status', 'author')
+    search_fields = ['title', 'content']
+    summernote_fields = ('content',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    """This class is for """
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'post', 'approved', 'created_date')
+    list_filter = ('post', 'approved')
+    search_fields = ['name', 'post']
+
+
+admin.site.register(Category)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post, PostAdmin)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """This class is for users to contact the admin"""
+    name = models.CharField(max_length=60)
+    email = models.EmailField()
+    subject = models.CharField(max_length=60)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.name
+
+
+class Newsletter(models.Model):
+    """This class is for website newsletter"""
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
