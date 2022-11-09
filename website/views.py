e798cda5402b03d446264a60dce72722e0ab2a6b
Amir Hamidi <Amirarsalanh2001@gmail.com>
2022-11-09 12:46:33 +0330

    django fbv

--- website/views.py
+++ website/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, "website/index.html")
+
+
+def about_view(request):
+    return render(request, "website/about.html")
+
+
+def contact_view(request):
+    return render(request, "website/contact.html")
+
+
+def detail_view(request):
+    return render(request, "website/portfolio-details.html")
+
+
+def portfolio_view(request):
+    return render(request, "website/portfolio.html")
+
+
+def services_view(request):
+    return render(request, "website/services.html")
+
+
+def team_view(request):
+    return render(request, "website/team.html")
